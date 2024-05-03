20eed22787acc37f792a881bbf01651e1dcff560
MaolanaF <maolana.firmansyah.tif421@polban.ac.id>
2024-05-03 17:53:12 +0700

    initialize maven project

---
+++ src/main/java/com/a3/api_testing/App.java
@@ -0,0 +1,13 @@
+package com.a3.api_testing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
